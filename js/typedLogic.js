327dd6b418ab08f180d402f2ac4feb9be691f36b
Kevin Saechew <kevinsaechew@gmail.com>
2020-06-18 21:10:07 -0400

    Updated Typed.js

--- js/typedLogic.js
+++ js/typedLogic.js
@@ -1,7 +1,9 @@
 function initType() {
 
 var typed = new Typed(document.getElementsByClassName('typed')[0], {
-    strings: ["(╯°□°)╯︵ ┻━┻!!!!", "X Æ A-12",
+    strings: [
+
+        "(╯°□°)╯︵ ┻━┻!!!!", "X Æ A-12",
         "(>'-')> (>'-')>(>'-')>(>'-')> (>'-')>(>'-')> (>'-')>  &#127833; &#127833; &#127833;",
         "<pre>/\\                 /\\\n" +
         "/ \\'._   (\\_/)   _.'/ \\\n" +
@@ -12,8 +14,17 @@ var typed = new Typed(document.getElementsByClassName('typed')[0], {
         "  \"  \"  </pre>",
 
 
-        "☜(⌒ ▽ ⌒)☞"],
-    typeSpeed: 80,
+        "☜(⌒ ▽ ⌒)☞",
+
+        "<pre>{\\__/}\n" +
+        "(•.•)\n" +
+        "             />🍿; u want this?\n" +
+        "\n" +
+        "{\\__/}\n" +
+        "(•.•)\n" +
+        "               />🌽; it\'s my bitcorn\n " +
+        "lol </pre> "],
+    typeSpeed: 60,
     backSpeed: 40,
     smartBackspace: true, // this is a default
     loop: true
